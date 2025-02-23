e754440c28e177ed6398972ad8442c7cdc413c51
Abdulrahman.Ali19 <abdulrahman.ali19@outlook.com>
2025-02-23 16:46:16 +0200

    add document.title

--- composeApp/src/wasmJsMain/kotlin/abdulrahman/ali19/main.kt
+++ composeApp/src/wasmJsMain/kotlin/abdulrahman/ali19/main.kt
@@ -4,11 +4,13 @@ import abdulrahman.ali19.di.initKoin
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     initKoin()
     ComposeViewport(document.body!!) {
+        window.document.title = "Abdulrahman Ali"
         App()
     }
 }
